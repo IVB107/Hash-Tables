778ff21f20e17e25a180c7ffa14a540a1b8d969e
IVB107 <ianvb107@gmail.com>
2019-07-08 17:23:05 -0500

    Implemented insert, delete and lookup methods in b_hashtables.py

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -7,6 +7,7 @@ class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.next = None
 
 
 # '''
@@ -15,7 +16,7 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        self.bucket_array = [None for i in range(capacity)]
+        self.storage = [None for i in range(capacity)]
         self.capacity = capacity
 
 
@@ -23,35 +24,75 @@ class BasicHashTable:
 # Fill this in.
 # Research and implement the djb2 hash function
 # '''
-def hash(string, max):
-    pass
-
+def hash(string):
+    hash_num = 5381
+    for char in string:
+        hash_num = (hash_num * 33) % ord(char)
+    return hash_num
 
 # '''
 # Fill this in.
-
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    key_hash = hash(key)
+    bucket_index = key_hash % hash_table.capacity
+
+    new_pair = Pair(key, value)
+    existing_pair = hash_table.storage[bucket_index]
+
+    if existing_pair:
+        last_pair = None
+        while existing_pair:
+            if existing_pair.key == key:
+                # Found existing key, replace value
+                existing_pair.value = value
+                return
+            last_pair = existing_pair
+            existing_pair = existing_pair.next
+        
+        # Chain new_pair to end of bucket's linked-list
+        last_pair.next = new_pair
+    else:
+        hash_table.storage[bucket_index] = new_pair
 
 
 # '''
 # Fill this in.
-
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    key_hash = hash(key)
+    bucket_index = key_hash % hash_table.capacity
+
+    existing_pair = hash_table.storage[bucket_index]
+    if existing_pair:
+        last_pair = None
+        while existing_pair:
+            if existing_pair.key == key:
+                if last_pair:
+                    last_pair.next = existing_pair.next
+                else:
+                    hash_table.storage[bucket_index] = existing_pair.next
+            last_pair = existing_pair
+            existing_pair = existing_pair.next
 
 
 # '''
 # Fill this in.
-
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    key_hash = hash(key)
+    bucket_index = key_hash % hash_table.capacity
+
+    existing_pair = hash_table.storage[bucket_index]
+    if existing_pair:
+        while existing_pair:
+            if existing_pair.key == key:
+                return existing_pair.value
+            existing_pair = existing_pair.next
+    return None
 
 
 def Testing():
